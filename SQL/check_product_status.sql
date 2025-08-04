6092b35554f1c5d63e3378c6996782113d3f3c46
pitipume <pitipume.boonyawan@gmail.com>
2025-08-04 21:58:54 +0700

    feat(product): add sql but fail syntax

--- SQL/check_product_status.sql
+++ SQL/check_product_status.sql
@@ -1,1 +1,17 @@
-﻿
+﻿CREATE OR ALTER PROCEDURE check_product_status
+    @p_name NVARCHAR(255),
+    @p_today DATE
+AS
+BEGIN
+    SELECT
+        p.Name AS product_name,
+        DATEDIFF(day, p.ManufactureDate, @p_today) AS aging_days,
+        CASE
+            WHEN p.ExpiryDate < @p_today THEN 1
+            ELSE 0
+        END AS is_expired
+    FROM
+        Products p
+    WHERE
+        p.Name = @p_name;
+END;
